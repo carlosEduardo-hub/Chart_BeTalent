8bb95e3de189ec0806a8cba1af694a76daf440df
carlosEduardo-hub <carloseduardo010616@gmail.com>
2025-03-01 19:53:38 -0300

    Aplication main section

---
+++ src/components/main_section.tsx
@@ -0,0 +1,9 @@
+import { SearchSection } from "./search_section";
+
+export function MainSection() {
+ return (
+   <main className=" min-w-[1024px] h-screen">
+    <SearchSection />
+   </main>
+ );
+}
---
+++ src/components/search_section.tsx
@@ -0,0 +1,7 @@
+export function SearchSection() {
+ return (
+   <div className=" max-w-[1024px] ml-40 mt-40 h-auto">
+    <h1 className="text-xl font-roboto font-medium text-black-neutral ">Funcionários</h1>
+   </div>
+ );
+}
