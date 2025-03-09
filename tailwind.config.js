6fb95c77943c1ce6065409f41169c35ea9a597eb
carlosEduardo-hub <carloseduardo010616@gmail.com>
2025-03-09 03:41:07 -0300

    bugfix: talwind version

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,41 +1,89 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [
+    darkMode: ["class"],
+    content: [
     "./index.html",
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {
-      fontFamily: {
-        'roboto': ['Roboto'],
-      },
-      colors: {
-        'blue-primary': '#0500FF',
-        'blue-10': '#EDEFFB',
-        'black-neutral': '#1C1C1C',
-        'gray-20': '#9E9E9E',
-        'gray-10': '#DFDFDF',
-        'gray-05': '#F5F5F5',
-        'gray-00': '#F0F0F0',
-      },
-      spacing: {
-        '80': '80px',
-        '60': '60px',
-        '40': '40px',
-        '32': '32px',
-        '28': '28px',
-        '20': '20px',
-        '16': '16px',
-        '8': '8px',
-        '4': '4px',
-      },
-      boxShadow: {
-        'shadow1': '0px 1px 2px 0px #00000033',
-        'shadow2': '0px 2px 4px 0px #00000033',
-        'shadow-small': '0px 2px 8px 0px #00000033',
-      },
-    },
+  	extend: {
+  		fontFamily: {
+  			roboto: [
+  				'Roboto'
+  			]
+  		},
+  		colors: {
+  			'blue-primary': '#0500FF',
+  			'blue-10': '#EDEFFB',
+  			'black-neutral': '#1C1C1C',
+  			'gray-20': '#9E9E9E',
+  			'gray-10': '#DFDFDF',
+  			'gray-05': '#F5F5F5',
+  			'gray-00': '#F0F0F0',
+  			background: 'hsl(var(--background))',
+  			foreground: 'hsl(var(--foreground))',
+  			card: {
+  				DEFAULT: 'hsl(var(--card))',
+  				foreground: 'hsl(var(--card-foreground))'
+  			},
+  			popover: {
+  				DEFAULT: 'hsl(var(--popover))',
+  				foreground: 'hsl(var(--popover-foreground))'
+  			},
+  			primary: {
+  				DEFAULT: 'hsl(var(--primary))',
+  				foreground: 'hsl(var(--primary-foreground))'
+  			},
+  			secondary: {
+  				DEFAULT: 'hsl(var(--secondary))',
+  				foreground: 'hsl(var(--secondary-foreground))'
+  			},
+  			muted: {
+  				DEFAULT: 'hsl(var(--muted))',
+  				foreground: 'hsl(var(--muted-foreground))'
+  			},
+  			accent: {
+  				DEFAULT: 'hsl(var(--accent))',
+  				foreground: 'hsl(var(--accent-foreground))'
+  			},
+  			destructive: {
+  				DEFAULT: 'hsl(var(--destructive))',
+  				foreground: 'hsl(var(--destructive-foreground))'
+  			},
+  			border: 'hsl(var(--border))',
+  			input: 'hsl(var(--input))',
+  			ring: 'hsl(var(--ring))',
+  			chart: {
+  				'1': 'hsl(var(--chart-1))',
+  				'2': 'hsl(var(--chart-2))',
+  				'3': 'hsl(var(--chart-3))',
+  				'4': 'hsl(var(--chart-4))',
+  				'5': 'hsl(var(--chart-5))'
+  			}
+  		},
+  		spacing: {
+  			'4': '4px',
+  			'8': '8px',
+  			'16': '16px',
+  			'20': '20px',
+  			'28': '28px',
+  			'32': '32px',
+  			'40': '40px',
+  			'60': '60px',
+  			'80': '80px'
+  		},
+  		boxShadow: {
+  			shadow1: '0px 1px 2px 0px #00000033',
+  			shadow2: '0px 2px 4px 0px #00000033',
+  			'shadow-small': '0px 2px 8px 0px #00000033'
+  		},
+  		borderRadius: {
+  			lg: 'var(--radius)',
+  			md: 'calc(var(--radius) - 2px)',
+  			sm: 'calc(var(--radius) - 4px)'
+  		}
+  	}
   },
-  plugins: [],
+  plugins: [require("tailwindcss-animate")],
 }
 
